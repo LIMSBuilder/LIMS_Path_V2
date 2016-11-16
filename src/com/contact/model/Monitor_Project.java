add2172445edd81652fb1a8e311d0513eb724e7b
瞿龙俊 <qulongjun@qulongjundeMacBook-Pro.local>
2016-11-16 13:06:49 +0800

    修复部分bug

--- src/com/contact/model/Monitor_Project.java
+++ src/com/contact/model/Monitor_Project.java
@@ -5,7 +5,7 @@ import com.jfinal.plugin.activerecord.Model;
 /**
  * 监测项目
  */
-public class Monitor_Project extends Model<Monitor_Project> {
+public class Monitor_Project extends Model<Monitor_Project> implements Comparable<Monitor_Project> {
     public static final Monitor_Project monitor_projectDao = new Monitor_Project();
 
     /**
@@ -15,5 +15,16 @@ public class Monitor_Project extends Model<Monitor_Project> {
      */
     public Monitor_Category getCategory() {
         return Monitor_Category.monitorCategoryDao.findById(get("category_id"));
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        return this.get("id") == ((Monitor_Project) o).get("id");
+    }
+
+    @Override
+    public int compareTo(Monitor_Project o) {
+        return this.getInt("id") - o.getInt("id");
     }
 }
